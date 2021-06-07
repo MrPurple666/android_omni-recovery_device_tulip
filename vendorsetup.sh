3a86d9df307e1802977aa2c0553bd054e8e1469a
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-06-07 03:11:25 +0000

    Now Miui OTA is default

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -46,7 +46,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export FOX_R11=1
         export FOX_ADVANCED_SECURITY=1
         export OF_USE_TWRP_SAR_DETECT=1
-        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
         export OF_QUICK_BACKUP_LIST="/boot;"
         # -- end R11 settings --
 
